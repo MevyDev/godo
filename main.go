0303b1dd104f510195374c13d0a1e5e055cb1bcf
MevyDev <mevydev@proton.me>
2025-08-04 20:41:17 +0200

    color constants

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"sort"
 )
 
+const (
+    Reset  = "\033[0m"
+    Red    = "\033[31m"
+    Green  = "\033[32m"
+    Yellow = "\033[33m"
+    Blue   = "\033[34m"
+)
+
 type task struct {
 	Text		string	`json:"text"`
 	Status		string	`json:"status"`
