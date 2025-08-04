4529caa5728f88c8d712289094139b34498084c0
MevyDev <mevydev@proton.me>
2025-08-04 16:40:10 +0200

    load tasks from file

--- main.go
+++ main.go
@@ -15,3 +15,25 @@ type task struct {
 type taskList struct {
 	Tasks []task `json:"tasks"`
 }
+
+
+func loadTasks(todoPath string) (taskList, error) {
+	data, err := os.ReadFile(todoPath)
+	if err != nil {
+		return taskList{Tasks: []task{}}, nil
+	}
+
+	var todos taskList
+	err = json.Unmarshal(data, &todos)
+	return todos, err
+}
+
+func main() {
+	var todoPath string = "todo.json"
+	var todoList taskList
+	todoList, err := loadTasks(todoPath)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(todoList.Tasks)
+}
